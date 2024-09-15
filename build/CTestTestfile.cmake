548e0a396065fae7aaea12e44faf88e04afe16ec
sfaisalali <sfaisal.ali91@gmail.com>
2024-09-16 03:02:26 +0530

    first commit

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Work/RamSoftTest
-# Build directory: E:/Work/RamSoftTest/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
--- build/_deps/gtest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Work/RamSoftTest/build/_deps/gtest-src/googletest
-# Build directory: E:/Work/RamSoftTest/build/_deps/gtest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
